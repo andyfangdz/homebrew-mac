c7d2365d77f2521e533bf0cadfd0c035c658cac9
Dezhi “Andy” Fang <andyfang.dz@gmail.com>
2018-05-22 10:06:44 -0700

    Update ripgrep-avx.rb

--- ripgrep-avx.rb
+++ ripgrep-avx.rb
@@ -1,10 +1,10 @@
 class RipgrepAvx < Formula
-  version 'a383d5c'
+  version '0.8.1-a383d5c'
   desc "Recursively search directories for a regex pattern. AVX Enabled"
   homepage "https://github.com/BurntSushi/ripgrep"
 
   if OS.mac?
-      url "https://bintray.com/andyfangdz/homebrew-mac/download_file?file_path=ripgrep-a383d5c-x86_64-apple-darwin.tar.gz"
+      url "https://dl.bintray.com/andyfangdz/homebrew-mac/ripgrep-a383d5c-x86_64-apple-darwin.tar.gz"
       sha256 "817f68dd140a748d7a2bc39c77419d00749f81aa672edc9bbe672f3c6ecedb3f"
   # elsif OS.linux?
   #     url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
